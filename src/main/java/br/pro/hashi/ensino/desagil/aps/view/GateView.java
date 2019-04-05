07296b97515b86b4ca503ccabd27a4996dd91147
Brunoic <bruno@pnl.com.br>
2019-04-05 12:55:03 -0300

    Trabalho de APS realizado por Bruno Cury e Felipe Aron

--- src/main/java/br/pro/hashi/ensino/desagil/aps/view/GateView.java
+++ src/main/java/br/pro/hashi/ensino/desagil/aps/view/GateView.java
@@ -1,4 +1,97 @@
 package br.pro.hashi.ensino.desagil.aps.view;
 
-public class GateView {
+import br.pro.hashi.ensino.desagil.aps.model.Gate;
+import br.pro.hashi.ensino.desagil.aps.model.Switch;
+
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+// A classe JPanel representa uma das componentes mais
+// simples da Swing. A função dela é simplesmente ser
+// um contêiner para colocar outras componentes dentro.
+// A razão de implementar ActionListener está mais abaixo.
+public class GateView extends JPanel implements ActionListener {
+
+    // A ideia é que essa componente gráfica represente
+    // uma calculadora específica. Essa calculadora que
+    // está sendo representada é guardada como atributo.
+    private final Gate gate;
+
+    // A classe JTextField representa um campo de texto.
+    private final JCheckBox[] inputs;
+    private final JCheckBox output;
+    private final Switch[] sws;
+
+    public GateView(Gate gate) {
+        this.gate = gate;
+        int g_size = gate.getInputSize();
+
+
+        JLabel entrada_label;
+        if (g_size>1) {
+            entrada_label = new JLabel("Entradas");
+        }
+        else{
+            entrada_label = new JLabel("Entrada");
+        }
+
+        inputs = new JCheckBox[g_size];
+        sws = new Switch[g_size];
+        output = new JCheckBox();
+
+        setLayout(new BoxLayout(this, BoxLayout.Y_AXIS));
+
+        add(entrada_label);
+
+        for (int i=0; i<g_size; i++){
+            inputs[i] = new JCheckBox();
+            add(inputs[i]);
+            inputs[i].addActionListener(this);
+
+
+            sws[i] = new Switch();
+            gate.connect(i, sws[i]);
+        }
+
+        JLabel saida_label = new JLabel("Saída");
+        add(saida_label);
+        add(output);
+        // O último campo de texto não pode ser editável, pois é
+        // só para exibição. Logo, configuramos como desabilitado.
+        output.setEnabled(false);
+
+        // Update é o método que definimos abaixo para atualizar o
+        // último campo de acordo com os valores dos primeiros.
+        // Precisamos chamar esse método no final da construção
+        // para garantir que a interface não nasce inconsistente.
+        update();
+    }
+
+    private void update() {
+//        for (int i=0; i<inputs.length; i++){
+//            sws[i].turnOff();
+//            if (inputs[i].isSelected()){
+//                sws[i].turnOn();
+//            }
+//        }
+
+        for (int i = 0; i < inputs.length; i++) {
+            if (inputs[i].isSelected()) {
+                sws[i].turnOn();
+            } else {
+                sws[i].turnOff();
+            }
+        }
+        output.setSelected(gate.read());
+
+//        output.setSelected(gate.read());
+    }
+
+    // O que esta componente deve fazer quando o usuário der um
+    // Enter depois de digitar? Apenas chamar o update, é claro!
+    @Override
+    public void actionPerformed(ActionEvent event) {
+        update();
+    }
 }
--- src/main/java/br/pro/hashi/ensino/desagil/aps/view/View.java
+++ src/main/java/br/pro/hashi/ensino/desagil/aps/view/View.java
@@ -1,4 +1,65 @@
 package br.pro.hashi.ensino.desagil.aps.view;
 
-public class View {
+import br.pro.hashi.ensino.desagil.aps.model.Gate;
+
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.LinkedList;
+
+public class View extends JPanel implements ActionListener {
+
+    private final JComboBox<Gate> menu;
+    private GateView gateview;
+
+    // O construtor recebe uma lista de calculadoras, que
+    // devem ser adicionadas ao menu. O menu consegue mostrar
+    // os nomes das calculadoras graças ao método toString.
+    public View(LinkedList<Gate> model) {
+        menu = new JComboBox<>();
+        for (Gate g : model) {
+            menu.addItem(g);
+        }
+
+        setLayout(new BoxLayout(this, BoxLayout.Y_AXIS));
+
+        add(menu);
+        addGateView(0);
+
+        menu.addActionListener(this);
+    }
+
+    // Este método é responsável por adicionar a este JPanel a
+    // representação gráfica da calculadora identificada por
+    // um índice. Isso consiste em três passos simples.
+    private void addGateView(int index) {
+
+        // 1. Usar o índice para pegar a calculadora do menu.
+        Gate gate= menu.getItemAt(index);
+
+        // 2. Construir a representação gráfica a partir dela.
+        gateview = new GateView(gate);
+
+        // 3. Adicionar essa representação gráfica no JPanel.
+        add(gateview);
+    }
+
+    // O que esta componente deve fazer quando o usuário
+    // selecionar um item no menu? Bem, ela deve...
+    @Override
+    public void actionPerformed(ActionEvent event) {
+
+        // ...tirar a atual representação gráfica de calculadora...
+        remove(gateview);
+
+        // ...descobrir qual é o índice da calculadora selecionada...
+        int index = menu.getSelectedIndex();
+
+        // ...e usar o método acima para adicionar a nova.
+        addGateView(index);
+
+        // Mantenha esta linha, mas não precisa entendê-la.
+        // É necessária para evitar bugs em alguns sistemas.
+        ((JFrame) SwingUtilities.getRoot(this)).pack();
+    }
 }
